78f3de180e27b34be45398215840965bf1ce3b04
Ross Burton <ross@linux.intel.com>
2009-03-25 12:06:53 +0000

    Add include guards

--- service-info.h
+++ service-info.h
@@ -1,3 +1,6 @@
+#ifndef _SERVICE_INFO_H
+#define _SERVICE_INFO_H
+
 typedef enum {
   AUTH_INVALID = 0,
   AUTH_USERNAME,
@@ -14,3 +17,5 @@ typedef struct {
 } ServiceInfo;
 
 ServiceInfo * get_info_for_service (const char *name);
+
+#endif /* _SERVICE_INFO_H */
