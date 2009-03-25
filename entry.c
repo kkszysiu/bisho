6646e0cb3c63c60d610a363b2a7412dd82125074
Ross Burton <ross@linux.intel.com>
2009-03-25 14:04:22 +0000

    Change key storage semantics

--- entry.c
+++ entry.c
@@ -7,17 +7,10 @@ static GConfClient *gconf = NULL;
 #define DATA_GCONF_KEY "bisho:gconf-key"
 
 static void
-set_gconf_key (ServiceInfo *info, const char *key_suffix, const char *value)
+set_gconf_key (ServiceInfo *info, const char *key, const char *value)
 {
-  char *key;
-
-  key = g_strdup_printf ("/apps/mojito/services/%s/%s",
-                         info->name, key_suffix);
-
   /* TODO: block gconf notify when we have one */
   gconf_client_set_string (gconf, key, value, NULL);
-
-  g_free (key);
 }
 
 static gboolean
@@ -33,13 +26,10 @@ on_gconf_entry_left (GtkWidget *widget, GdkEventFocus *event, gpointer user_data
 }
 
 static char *
-get_gconf_key (ServiceInfo *info, const char *key_suffix)
+get_gconf_key (ServiceInfo *info, const char *key)
 {
-  char *key, *value;
+  char *value;
   GError *error = NULL;
-
-  key = g_strdup_printf ("/apps/mojito/services/%s/%s",
-                         info->name, key_suffix);
 
   value = gconf_client_get_string (gconf, key, &error);
   if (error) {
@@ -47,24 +37,29 @@ get_gconf_key (ServiceInfo *info, const char *key_suffix)
     g_error_free (error);
   }
 
-  g_free (key);
-
   return value;
 }
 
 GtkWidget *
-new_entry_from_gconf (ServiceInfo *info, const char *key)
+new_entry_from_gconf (ServiceInfo *info, const char *key_suffix)
 {
   GtkWidget *entry;
+  char *key, *value;
 
   g_assert (info);
   g_assert (key);
 
   if (!gconf) gconf = gconf_client_get_default ();
 
+  key = g_strdup_printf ("/apps/mojito/services/%s/%s", info->name, key_suffix);
+
   entry = gtk_entry_new ();
-  g_object_set_data (G_OBJECT (entry), DATA_GCONF_KEY, (gpointer)key);
-  gtk_entry_set_text (GTK_ENTRY (entry), get_gconf_key (info, key));
+  g_object_set_data_full (G_OBJECT (entry), DATA_GCONF_KEY, key, g_free);
+
+  value = get_gconf_key (info, key);
+  gtk_entry_set_text (GTK_ENTRY (entry), value);
+  g_free (value);
+
   /* TODO: connect to gconf notify */
   g_signal_connect (entry, "focus-out-event", G_CALLBACK (on_gconf_entry_left), info);
 
