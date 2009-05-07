ecab759445da255ee2c3a3722d81e7697f23af32
Ross Burton <ross@linux.intel.com>
2009-05-07 14:51:04 +0100

    Get a request token and let the user login. Just the hard bit left...

--- bisho-oauth-pane.c
+++ bisho-oauth-pane.c
@@ -1,6 +1,8 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 #include <gnome-keyring.h>
+#include <libsoup/soup.h>
+#include <rest/oauth-proxy.h>
 #include "service-info.h"
 #include "bisho-oauth-pane.h"
 
@@ -14,10 +16,14 @@ static const GnomeKeyringPasswordSchema oauth_schema = {
   }
 };
 
+/* TODO: make this a widget subclass and this private data */
 typedef struct {
+  ServiceInfo *info;
   GtkWidget *label;
   GtkWidget *button;
 } WidgetData;
+
+static void update_widgets (WidgetData *data, gboolean logged_in);
 
 static GtkWidget *
 make_disclaimer_label (ServiceInfo *info)
@@ -35,15 +41,88 @@ make_disclaimer_label (ServiceInfo *info)
   return label;
 }
 
+static char *
+create_url (ServiceInfo *info, const char *token)
+{
+  SoupURI *base, *uri;
+  char *s;
+
+  g_assert (info);
+  g_assert (info->oauth.callback);
+  g_assert (token);
+
+  base = soup_uri_new (info->oauth.base_url);
+  uri = soup_uri_new_with_base (base, info->oauth.authorize_function);
+  soup_uri_free (base);
+
+  soup_uri_set_query_from_fields (uri,
+                                  "oauth_token", token,
+                                  "oauth_callback", info->oauth.callback,
+                                  NULL);
+
+  s = soup_uri_to_string (uri, FALSE);
+  soup_uri_free (uri);
+  return s;
+}
+
+static void
+log_in_clicked (GtkButton *button, gpointer user_data)
+{
+  WidgetData *data = user_data;
+  ServiceInfo *info = data->info;
+  RestProxy *proxy;
+  GError *error = NULL;
+  char *url;
+
+  proxy = oauth_proxy_new (info->oauth.consumer_key, info->oauth.consumer_secret, info->oauth.base_url, FALSE);
+
+  /* TODO: async and change button title/disable*/
+  oauth_proxy_auth_step (OAUTH_PROXY (proxy), info->oauth.request_token_function, &error);
+  if (error) {
+    /* TODO */
+    g_warning ("%s", error->message);
+    return;
+  }
+
+  url = create_url (info, oauth_proxy_get_token (OAUTH_PROXY (proxy)));
+  gtk_show_uri (gtk_widget_get_screen (GTK_WIDGET (button)), url, GDK_CURRENT_TIME, NULL);
+
+  /* TODO wait for dbus call from callback. For now pop up a lame dialog or something */
+}
+
+
+static void
+delete_done_cb (GnomeKeyringResult result, gpointer user_data)
+{
+  WidgetData *data = user_data;
+  update_widgets (data, result != GNOME_KEYRING_RESULT_OK);
+}
+
+static void
+log_out_clicked (GtkButton *button, gpointer user_data)
+{
+  WidgetData *data = user_data;
+
+  gnome_keyring_delete_password (&oauth_schema, delete_done_cb, user_data, NULL,
+                                 "server", data->info->oauth.base_url,
+                                 "consumer-key", data->info->oauth.consumer_key,
+                                 NULL);
+}
+
 static void
 update_widgets (WidgetData *data, gboolean logged_in)
 {
+  g_signal_handlers_disconnect_by_func (data->button, log_out_clicked, data);
+  g_signal_handlers_disconnect_by_func (data->button, log_in_clicked, data);
+
   if (logged_in) {
     gtk_label_set_text (GTK_LABEL (data->label), _("Logged in"));
     gtk_button_set_label (GTK_BUTTON (data->button), _("Log me out"));
+    g_signal_connect (data->button, "clicked", G_CALLBACK (log_out_clicked), data);
   } else {
     gtk_label_set_text (GTK_LABEL (data->label), _("Log in pending"));
     gtk_button_set_label (GTK_BUTTON (data->button), _("Log me in"));
+    g_signal_connect (data->button, "clicked", G_CALLBACK (log_in_clicked), data);
   }
 }
 
@@ -66,6 +145,7 @@ bisho_oauth_pane_new (ServiceInfo *info)
   g_assert (info->auth == AUTH_OAUTH);
 
   data = g_slice_new0 (WidgetData);
+  data->info = info;
 
   table = gtk_table_new (2, 2, TRUE);
 
@@ -91,7 +171,7 @@ bisho_oauth_pane_new (ServiceInfo *info)
   update_widgets (data, FALSE);
 
   gnome_keyring_find_password (&oauth_schema, find_key_cb, data, NULL,
-                               "server", info->oauth.server,
+                               "server", info->oauth.base_url,
                                "consumer-key", info->oauth.consumer_key,
                                NULL);
 
